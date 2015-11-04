19658a50bcb8df2219f7d123d1a9bd66e5a05445
Ian Stirton <stirtoni@msu.edu>
2015-11-04 11:00:50 -0500

    ATA ( Super Rough Demo)

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+ $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
